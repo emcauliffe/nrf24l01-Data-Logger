264c6d8f7764173ec5a4fd2f99ef33681fc521b1
Timothy John Morland <thetimmorland@gmail.com>
2017-03-10 13:37:36 -0500

    Internal EEPROM storage and bargraph display
    data is now stored in the UNO’s internal EEPROM, rather than an I2C
    device. LED bargraph displays the temperature range.

--- unoReceiving/unoReceiving.ino
+++ unoReceiving/unoReceiving.ino
@@ -4,16 +4,24 @@
 #include <RF24_config.h>
 #include <Wire.h>
 
-#define DISK1 0x50 //address of EEPROM chip
 #define CHANNEL 83
 
-uint8_t inputData;
+#define DATAPIN 4
+#define LATCHPIN 3
+#define CLKPIN 2
+
+uint8_t receivedData;
+uint8_t displayData;
 uint16_t address = 0;
 
 RF24 radio(7, 8);
 uint8_t wirelessAddresses[][6] = {"1Node", "2Node"};
 
 void setup() {
+  pinMode(DATAPIN, OUTPUT);
+  pinMode(LATCHPIN, OUTPUT);
+  pinMode(CLKPIN, OUTPUT);
+
   Wire.begin();
   Serial.begin(9600);
   radio.begin();
@@ -27,10 +35,12 @@ void setup() {
 }
 
 void loop() {
-  inputData = readWireless();
-  writeEEPROM(DISK1, address, inputData);
-  address++;
+  receivedData = readWireless();
+  Serial.println(receivedData);
+  displayData = map(receivedData, 0, 255, 0, 8);
+  displayTemp(displayData);
 }
+
 uint8_t readWireless() {
   uint8_t received;
   while (radio.available()) {
@@ -38,11 +48,14 @@ uint8_t readWireless() {
   }
   return received;
 }
-void writeEEPROM(int16_t deviceaddress, uint16_t eeaddress, uint8_t data ) {
-  Wire.beginTransmission(deviceaddress);
-  Wire.write((int)(eeaddress >> 8));// MSB
-  Wire.write((int)(eeaddress & 0xFF));// LSB
-  Wire.write(data);
-  Wire.endTransmission();
-  delay(5);
+
+void displayTemp (uint8_t data) {
+  uint8_t shiftData = 0;
+  for (uint8_t n = 0; n < data; n ++) {
+    shiftData |= (1 << n);
+  }
+  shiftOut(DATAPIN, CLKPIN, LSBFIRST, shiftData);
+  digitalWrite(LATCHPIN, HIGH);
+  digitalWrite(LATCHPIN, LOW);
 }
+

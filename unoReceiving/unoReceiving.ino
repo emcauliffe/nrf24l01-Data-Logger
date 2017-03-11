1b3a1e72f9ea87dfddb8c4ec15a891ed28675d3c
Timothy John Morland <thetimmorland@gmail.com>
2017-03-11 14:13:48 +0000

    general cleaning and renaming of variables

--- unoReceiving/unoReceiving.ino
+++ unoReceiving/unoReceiving.ino
@@ -1,8 +1,9 @@
-#include <EEPROM.h>
 #include <nRF24L01.h>
-#include <printf.h>
 #include <RF24.h>
 #include <RF24_config.h>
+#include <printf.h>
+#include <Wire.h>
+#include <EEPROM.h>
 
 #define CHANNEL 106
 
@@ -11,17 +12,18 @@
 #define CLKPIN 2
 
 int8_t receivedData;
-uint16_t EEPROMAddress = 0;
+int8_t eepromAddress;
 
 RF24 radio(7, 8);
 uint8_t wirelessAddresses[][6] = {"1Node", "2Node"};
 
 void setup() {
-  //configure pins for shift register
+  
   pinMode(DATAPIN, OUTPUT);
   pinMode(LATCHPIN, OUTPUT);
   pinMode(CLKPIN, OUTPUT);
 
+  Wire.begin();
   Serial.begin(9600);
   radio.begin();
 
@@ -31,22 +33,34 @@ void setup() {
   radio.openWritingPipe(wirelessAddresses[1]);
   radio.openReadingPipe(1, wirelessAddresses[0]);
   radio.startListening();
+  
 }
-void loop() {  
+
+void loop() {
+
   if (radio.available()) {
+    
     radio.read(&receivedData, 1);
+    
     Serial.println(receivedData);
-    EEPROM.write(EEPROMAddress, receivedData);//write temperature to eeprom
-    EEPROMAddress++;
-    displayTemp(map(receivedData, -20, 80, 0, 8));//maps temperatures from -20C to 80C on the bar graph
+
+    if (eepromAddress == EEPROM.length()) eepromAddress = 0;
+    EEPROM.write(eepromAddress, receivedData);
+    eepromAddress++;
+    
+    displayTemp(map(receivedData, -20, 80, 0, 8));
+    
   }
 }
+
 void displayTemp (uint8_t data) {
+  
   uint8_t shiftData = 0;
-  for (uint8_t n = 0; n < data; n ++) {
-    shiftData |= (1 << n);
-  }
+
+  for (uint8_t n = 0; n < data; n ++) shiftData |= (1 << n);
+
   shiftOut(DATAPIN, CLKPIN, LSBFIRST, shiftData);
   digitalWrite(LATCHPIN, HIGH);
   digitalWrite(LATCHPIN, LOW);
+  
 }
